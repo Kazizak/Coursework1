d8407edebe7af621741ed60475aff89a08e970d8
Kazi <ka1064@live.mdx.ac.uk>
2021-11-19 04:19:59 +0000

    Seperate subjects file and shopping cart with remove button

---
+++ products.js
@@ -0,0 +1,55 @@
+let courses= [
+    {
+      id: 0,
+      subject: "Maths",
+      campus: "London",
+      price: "£100",
+      spaces: 5,
+      A_D: false
+      // image: "images/maths.png"
+    },
+    {
+      id: 1,
+      subject: "Economics",
+      campus: "Leeds",
+      price: "£95",
+      spaces: 5,
+      A_D: false
+      // image :"images/maths.png"
+    },
+    {
+      id: 3,
+      subject: "Computer Science",
+      campus: "Toronto",
+      price: "£115",
+      spaces: 5,
+      A_D: false
+      // image :"images/maths.png"
+    },
+    {
+      id: 4,
+      subject: "Data Science",
+      campus: "Mauritius",
+      price: "£99",
+      spaces: 5,
+      A_D: false
+      //   image :"images/maths.png"
+    },
+    {
+      id: 5,
+      subject: "Data Representation",
+      campus: "North Hampton",
+      price: "£85",
+      spaces: 5,
+      A_D: false
+      //  image :"images/maths.png"
+    },
+    {
+      id: 6,
+      subject: "Chemistry",
+      campus: "Lonodn",
+      price: "£125",
+      spaces: 5,
+      A_D: false,
+      // image: "images/maths.png"
+    }]
